88d15e5b2c5c68dedc189983b92d48e21d6860c7
ANANT0908 <87381952+ANANT0908@users.noreply.github.com>
2021-09-29 23:36:23 +0530

    ModWrapperFunc

---
+++ ModWrapperFunc/index.js
@@ -0,0 +1,19 @@
+
+
+( function(exports,require,module,__filename,__dirname){
+    
+    const a = require("fs");
+    const name  = "Anant";
+    console.log(name);
+    module.exports = {hfkerjd}
+} );
+
+
+(function(){
+    var a = "Anant Verma";
+    console.log(a);
+})();
+
+console.log(__dirname);
+console.log(__filename);
+
