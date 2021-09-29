f26bb8d821d96075f7c8eb372e3594e813e29ade
ANANT0908 <87381952+ANANT0908@users.noreply.github.com>
2021-09-29 20:11:20 +0530

    Chalk

---
+++ npmMod/index.js
@@ -0,0 +1,7 @@
+const chalk = require("chalk");
+const validator = require("validator");
+// console.log(chalk.red.underline.inverse("false"));
+
+const result =  validator.isEmail('foo@bar.com');
+
+console.log(result ? chalk.green.inverse(result) : chalk.red.inverse(result));
