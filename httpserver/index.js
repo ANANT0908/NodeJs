0acee6196ef929550fcc6c07d311c9814f0b7536
ANANT0908 <87381952+ANANT0908@users.noreply.github.com>
2021-09-30 20:37:07 +0530

    Handle http reqyues

--- httpserver/index.js
+++ httpserver/index.js
@@ -1,7 +1,20 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=>{
-    res.end('Hello from the other side');
+    // console.log(req.url);
+    if(req.url === "/"){
+        res.end('Hello from the Home side');
+    }
+    else if(req.url === "/about"){
+        res.end('Hello from the About side');
+    }
+    else if(req.url === "/contact"){
+        res.end('Hello from the Contact us side');
+    }
+    else{
+        res.writeHead(404 ,{"Content-types" : "text/html"});
+        res.end("<h1>404 Error</h1>")
+    }
 });
 
 server.listen(8000,'127.0.0.1',()=>{
