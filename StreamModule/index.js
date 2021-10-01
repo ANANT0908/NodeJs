1075b1f250c9d35392a9934e094d109fd59906fe
ANANT0908 <87381952+ANANT0908@users.noreply.github.com>
2021-10-01 20:33:05 +0530

    Stream Module

--- StreamModule/index.js
+++ StreamModule/index.js
@@ -6,23 +6,10 @@ const server  = http.createServer();
 
 server.on("request", (req,res)=>{
 
-    // fs.readFile('input.txt' , (err,data) => {
-    //     if(err) return console.log(err);
-
-    //    res.end(data.toString());
-    // })
-
     const rstream = fs.createReadStream("input.txt");
-    rstream.on("data" , (chunkData)=>{
-        res.write(chunkData);
-    });
-    rstream.on("end" ,()=>{
-        res.end();
-    });
-    rstream.on("error" ,(err)=>{
-        console.log(err);
-        res.end("file not found");
-    });
+    rstream.pipe(res);
+
+
 });
 
 server.listen(8000,"127.0.0.1");
